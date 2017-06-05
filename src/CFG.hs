71a471ab2cd2c8aacb684bd6c9905a751aca74e5
Quytelda Kahja <quytelda@tamalin.org>
2017-06-05 10:09:16 -0500

    Add Parsec parser for context free grammers.

--- src/CFG.hs
+++ src/CFG.hs
@@ -16,12 +16,13 @@
 -- You should have received a copy of the GNU General Public License
 -- along with babel.  If not, see <http://www.gnu.org/licenses/>.
 
-module CFG where
+module CFG (produce, expand, parseCFG) where
 
 import Prelude hiding (lookup)
 import Data.Maybe
 import Data.Map
 import System.Random
+import Text.ParserCombinators.Parsec
 
 data Symbol = Node String
             | Terminal String
@@ -44,6 +45,39 @@ expand cfg sym@(Node nt) = do
   symbols <- produce cfg sym
   concat <$> mapM (expand cfg) symbols
 expand cfg sym@(Terminal t) = return t
+
+-- Parsec parser for CFGs
+
+whitespace = many (oneOf " \t")
+
+cfg = endBy rules (char '\n')
+
+rules = do
+  _ <- whitespace
+  var <- variable
+  _ <- whitespace
+  opt <- options
+  return (var, opt)
+
+variable = do
+  _ <- whitespace
+  result <- symbol
+  _ <- whitespace
+  string "->"
+  _ <- whitespace
+  return result
+
+options = do
+  _ <- whitespace
+  sepBy symbol (char '|')
+symbol = do
+  _ <- whitespace
+  result <- many (noneOf "|->\n ")
+  _ <- whitespace
+  return result
+
+parseCFG :: String -> FilePath -> Either ParseError [(String, [String])]
+parseCFG input fp = parse cfg fp input
 
 {-| Randomly select an element from a list. -}
 pickRIO :: [a] -> IO (Maybe a)
