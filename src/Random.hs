b99e3659807a8fee29d645ca0efb5d23f1885d56
Quytelda Kahja <quytelda@tamalin.org>
2018-02-19 21:09:24 -0800

    Remove redundant constraint on randomCryptIO.

--- src/Random.hs
+++ src/Random.hs
@@ -1,6 +1,5 @@
 module Random where
 
-import System.Random
 import Crypto.Number.Generate
 
 {-| Randomly select an element from a list using the random number
@@ -14,7 +13,7 @@ pickRIO randRange xs = let minI = 0
                        in randRange (minI, maxI)
                           >>= return . Just . (xs !!)
 
-randomCryptIO :: (Random a, Integral a) => (a, a) -> IO a
+randomCryptIO :: Integral a => (a, a) -> IO a
 randomCryptIO (low, hi) = do
   integer <- generateBetween (toInteger low) (toInteger hi)
   return (fromIntegral integer)
