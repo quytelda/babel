e26030a16a0047e38c5f2f07dd0bed44d86867e1
Quytelda Kahja <quytelda@tamalin.org>
2018-02-19 20:03:56 -0800

    Generalize the type signature of pickRIO.

--- src/Random.hs
+++ src/Random.hs
@@ -1,13 +1,13 @@
 module Random where
 
-import Crypto.Number.Generate
 import System.Random
+import Crypto.Number.Generate
 
 {-| Randomly select an element from a list using the random number
 function randRange.  randRange must be able to select a random Int
 from a provided range.
 If the list is empty, this function returns Nothing. -}
-pickRIO :: ((Int, Int) -> IO Int) -> [a] -> IO (Maybe a)
+pickRIO :: (Monad  m) => ((Int, Int) -> m Int) -> [a] -> m (Maybe a)
 pickRIO randRange [] = return Nothing
 pickRIO randRange xs = let min = 0
                            max = length xs - 1
